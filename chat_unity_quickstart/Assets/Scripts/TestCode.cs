badacd127ae921e73f1fa97efdf606b661e6c085
Jiepeng Du <347302029@qq.com>
2022-05-13 18:11:03 +0800

    remove em methods.

--- chat_unity_quickstart/Assets/Scripts/TestCode.cs
+++ chat_unity_quickstart/Assets/Scripts/TestCode.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class TestCode : MonoBehaviour
@@ -58,8 +59,7 @@ public class TestCode : MonoBehaviour
     // 初始化聊天SDK
     private void InitSDK()
     {
-        var options = new Options(appKey: APPKEY);
-        SDKClient.Instance.InitWithOptions(options);
+ 
     }
 
     // 添加消息监听
@@ -114,78 +114,4 @@ public class TestCode : MonoBehaviour
         LogText.text += System.DateTime.Now +": " + str + "\n";
     }
 
-    public void OnMessagesReceived(List<Message> messages)
-    {
-        foreach (Message msg in messages) {
-            if (msg.Body.Type == MessageBodyType.TXT)
-            {
-                TextBody txtBody = msg.Body as TextBody;
-                AddLogToLogText($"received text message: {txtBody.Text}, from: {msg.From}");
-            }
-            else if (msg.Body.Type == MessageBodyType.IMAGE)
-            {
-                ImageBody imageBody = msg.Body as ImageBody;
-                AddLogToLogText($"received image message, from: {msg.From}");
-            }
-            else if (msg.Body.Type == MessageBodyType.VIDEO) {
-                VideoBody videoBody = msg.Body as VideoBody;
-                AddLogToLogText($"received video message, from: {msg.From}");
-            }
-            else if (msg.Body.Type == MessageBodyType.VOICE)
-            {
-                VoiceBody voiceBody = msg.Body as VoiceBody;
-                AddLogToLogText($"received voice message, from: {msg.From}");
-            }
-            else if (msg.Body.Type == MessageBodyType.LOCATION)
-            {
-                LocationBody localBody = msg.Body as LocationBody;
-                AddLogToLogText($"received location message, from: {msg.From}");
-            }
-            else if (msg.Body.Type == MessageBodyType.FILE)
-            {
-                FileBody fileBody = msg.Body as FileBody;
-                AddLogToLogText($"received file message, from: {msg.From}");
-            }
-        }
-    }
-
-    public void OnCmdMessagesReceived(List<Message> messages)
-    {
-        
-    }
-
-    public void OnMessagesRead(List<Message> messages)
-    {
-        
-    }
-
-    public void OnMessagesDelivered(List<Message> messages)
-    {
-        
-    }
-
-    public void OnMessagesRecalled(List<Message> messages)
-    {
-        
-    }
-
-    public void OnReadAckForGroupMessageUpdated()
-    {
-        
-    }
-
-    public void OnGroupMessageRead(List<GroupReadAck> list)
-    {
-        
-    }
-
-    public void OnConversationsUpdate()
-    {
-        
-    }
-
-    public void OnConversationRead(string from, string to)
-    {
-        
-    }
 }
